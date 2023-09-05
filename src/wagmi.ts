8346e53a18b8b045c8c02f5f678c8b87154ab73c
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2023-09-06 01:49:11 +0300

    init with an nft

---
+++ src/wagmi.ts
@@ -0,0 +1,16 @@
+import { getDefaultConfig } from "connectkit";
+import { createConfig } from "wagmi";
+import { sepolia } from 'wagmi/chains'
+
+const chains = [sepolia]
+
+export const wagmiConfig = createConfig(
+  getDefaultConfig({
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    // alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+    chains,
+    appName: 'Vite Tokenbound SDK Example',
+    appDescription: 'Tokenbound SDK Example',
+    appUrl: 'https://tokenbound.org',
+  })
+)
