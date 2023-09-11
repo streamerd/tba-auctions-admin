a87e72e47c2d15052d71ce85759f20b855e1b7bf
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2023-09-12 01:01:39 +0300

    browserrouter > hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { ConnectKitProvider } from "connectkit";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { WagmiConfig } from "wagmi";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { App } from "./App";
 import { wagmiConfig } from "./wagmi";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<BrowserRouter>
+	<HashRouter>
 		<WagmiConfig config={wagmiConfig}>
 			<ConnectKitProvider>
 				<App />
 			</ConnectKitProvider>
 		</WagmiConfig>
-	</BrowserRouter>
+	</HashRouter>
 );
