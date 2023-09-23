c083dd6735ed41c9214c7d54894671d6811a0fb0
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2023-09-24 02:17:46 +0300

    rm unused

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,13 @@
-// code a navbar component here and export
-
-// Path: src/components/Navbar.tsx
-// get wallet connection logic in here.
-import { useEthers6Signer } from "../../hooks";
-import { useCallback, useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { ConnectKitButton } from "connectkit";
 import {
-  LatterNavbarItemsContainer,
   NavbarContainer,
   NavbarLogo,
 } from "./NavbarStyled";
 import catLogo from "../../assets/cat-logo.png";
 import { Link } from "react-router-dom";
-import { ActionButton } from "../../pages/NFTDetails/NFTDetailsStyled";
-import { YouGotThisIcon } from "../YouGotThisIcon";
 
 export function Navbar() {
-  const adminWallet = "0xB56DC5EBEEc61e2c0667746F64FC916e262919c8";
-  const signer = useEthers6Signer({ chainId: 11155111 });
   const { isConnected, address } = useAccount();
   console.log(isConnected, address);
 
@@ -28,17 +17,7 @@ export function Navbar() {
         <NavbarLogo src={catLogo} alt="logo" />
       </Link>
 
-      {/* <LatterNavbarItemsContainer> */}
-      {/* {
-        address === adminWallet ? (
-
-        <Link to={"/my-nfts"} style={{ textDecoration: "none" }}>
-          <YouGotThisIcon />
-        </Link>): null
-      } */}
-
-        <ConnectKitButton />
-      {/* </LatterNavbarItemsContainer> */}
+      <ConnectKitButton />
     </NavbarContainer>
   );
 }
